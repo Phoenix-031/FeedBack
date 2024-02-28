86a1787298991917821d72a7322a49ac1d22c854
Debayan Pradhan <pradhandebayan@gmail.com>
2024-02-28 19:11:58 +0530

    alumni feedback base section

--- src/pages/Alumni/Alumni.tsx
+++ src/pages/Alumni/Alumni.tsx
@@ -1,7 +1,47 @@
+import styles from './alumni.module.scss';
+
+import { AlumniData } from '../../data/alumni';
 
 const Alumni = () => {
   return (
-    <div>Alumni</div>
+    <div className={styles.alumni__main__section}>
+      <div className={styles.alumni__heading__container}>
+        <p>Alumni's Feedback Form</p>
+      </div>
+
+      <div className={styles.alumni__sections__container}>
+
+        {AlumniData.map((alumnisection, index) => {
+          return (
+
+            <div>
+              <div>
+                <p>{alumnisection.sectionTitle}</p>
+              </div>
+
+              <div>
+                {alumnisection.questions.map((question, index) => {
+                  return (
+                    <li key={index}>
+                      <input
+                        type="radio"
+                        id={`option${index}`}
+                        name="quizOptions"
+                        value={question.options}
+                      />
+                      <div className={styles.custom__checkbox}></div>
+                      <label htmlFor={`option${index}`}>{question.question}</label>
+                    </li>
+                  )
+                })
+                }
+              </div>
+            </div>
+        )}
+        )}
+
+      </div>
+    </div>
   )
 }
 
