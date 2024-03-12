1827ed9d2cd15680b4634ca3062bf0c571d39979
Debayan Pradhan <pradhandebayan@gmail.com>
2024-03-12 16:33:54 +0530

    addedradio button

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,5 +1,9 @@
 
 import styles from './question.module.scss'
+import { useEffect, useState } from 'react'
+
+import type { RadioChangeEvent } from 'antd';
+import { Radio } from 'antd';
 
 interface QuestionsProps {
     data: {
@@ -10,30 +14,56 @@ interface QuestionsProps {
 }
 
 const Questions = (props :QuestionsProps) => {
+
+
+    // useEffect(() => {
+    //     console.log(selectedOption)
+    // },[selectedOption])
+
+  const [value, setValue] = useState(1);
+
+  const onChange = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
+  };
+    
   return (
     <div className={styles.question__main__container}>
         <div>
             <p>{props.data.question}</p>
         </div>
         <div>
-            {
-                props.data.options.map((option, index) => {
+            
+        <Radio.Group onChange={onChange} value={value}>
+
+               { props.data.options.map((option, index) => {
                     return (
-                    <li key={index}>
-                        <input
-                        type="radio"
-                        id={`option${index}`}
-                        name="quizOptions"
-                        value={option}
-                        //   checked={selectedOption === option}
-                        //   onChange={() => handleOptionSelect(option)}
-                        />
-                        <div className={styles.custom__checkbox}></div>
-                        <label htmlFor={`option${index}`}>{option}</label>
-                    </li>
+                        <div key={index}>
+                            {/* <Checkbox onChange={onChange}>{option}</Checkbox> */}
+                            <Radio value={option} style={{
+                                fontSize:'1.2rem'
+                            }}>{option}</Radio>
+                        </div>
+                  // <li key={index}>
+                    //     <input
+                    //     type="radio"
+                    //     id={`option${index}`}
+                    //     name="quizOptions"
+                    //     value={option}
+                    //     checked={selectedOption === option}
+                    //     onChange={() => {
+                    //         setSelectedOption(option)
+                    //     }}
+                    //     />
+                    //     <div className={styles.custom__checkbox}></div>
+                    //     <label htmlFor={`option${index}`}>{option}</label>
+                    // </li>
                     )
                 })
             }
+                            </Radio.Group>
+
+            
         </div>
     </div>
   )
